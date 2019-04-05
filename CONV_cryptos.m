a15d89cb16fda829b0fa28e3f00c98f89c6aaa91
Daniel Traian Pele <danpele@ase.ro>
2019-04-05 19:27:47 +0300

    Add files via upload

---
+++ CONV_cryptos.m
@@ -0,0 +1,113 @@
+%% Factor modelling of assets
+
+%% Global Commands
+
+clear;clc;
+
+% specify directoy for the files
+directory='D:\PROIECTE\berlin_2018\Final_code';
+addpath(genpath(directory))
+cd(directory)
+
+%set global commands for font size and line width
+size_font=9;
+size_line=1.5;
+set(0,'DefaultAxesFontSize',size_font,'DefaultTextFontSize',size_font);
+set(0,'defaultlinelinewidth',size_line)
+
+% figures
+set(0, 'defaultFigurePaperType', 'A4')
+set(0, 'defaultFigurePaperUnits', 'centimeters')
+set(0, 'defaultFigurePaperPositionMode', 'auto')
+figure_wide=[680 678 800 420];
+
+% colors
+color_blue=[0 102 204]./255;
+color_green=[0 204 102]./255;
+color_red=[204 0 0]./255;
+color_black=[0 0 0];
+
+% reset rngs before running
+rng(1)
+
+%% Data
+
+load stats_dynamic.mat
+
+
+
+%% Expanding window factor model
+
+t_start=ceil(t_max/3);
+user_factor=2;
+h=figure();
+
+% init
+%loadings=[];
+
+
+for j=[1:971]%(t_max-t_start)
+    time(j,1)=datetime(date_unique(j+t_start-1,1),'ConvertFrom','datenum');
+    % stats for one point in time
+    stats_t=stats(:,:,j);
+
+    % nan assets
+    assets_nan=sum(isnan(stats_t),2)>0;
+    stats_t(assets_nan,:)=[0];
+    
+    parms_nan=sum(isnan(stats_t),1)>0;
+    stats_t(:,parms_nan)=[0];
+    Rho=corr(stats_t);
+    n1 = length(stats_t);
+    m  = mean(stats_t);
+    zz = (stats_t - repmat(m,n1,1))./repmat(sqrt(var(stats_t)), n1, 1);
+
+    F=transpose(f2*transpose(zz));
+    F(:,2)=-F(:,2);
+
+type_crypto=strcmp(type_assets,'Crypto');
+type_crypto_mod=type_crypto+1;
+for i=1
+    [est_reg_para,est_reg_dev,est_reg_stats]=mnrfit(F(:,1),type_crypto_mod);
+end
+
+    Dev_F1(j,1)=est_reg_dev;
+for i=2
+    [est_reg_para,est_reg_dev,est_reg_stats]=mnrfit(F(:,2),type_crypto_mod);
+end
+    Dev_F2(j,1)=est_reg_dev;
+for i=3
+    [est_reg_para,est_reg_dev,est_reg_stats]=mnrfit(F(:,3),type_crypto_mod);
+end
+    Dev_F3(j,1)=est_reg_dev;
+ 
+   
+end
+y_lim=[0 140];
+
+
+ax1 = subplot(3,1,1); % top subplot
+
+
+plot(time,Dev_F1);
+%ylim(y_lim);
+ylabel('Likelihood Ratio');
+xlabel('Time');
+title(ax1,'Tail Factor');
+
+ax2 = subplot(3,1,2); % bottom subplot
+plot( time,Dev_F2);
+%ylim(y_lim);
+ylabel('Likelihood Ratio');
+xlabel('Time');
+
+title(ax2,'Moment Factor')
+
+ax3 = subplot(3,1,3); % bottom subplot
+plot(time,Dev_F3);
+%ylim(y_lim);
+ylabel('Likelihood Ratio');
+xlabel('Time');
+
+title(ax3,'Memory Factor');
+
